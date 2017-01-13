e6c61495672a8d498d83068c2962dae7bfa7b945
Peter Salomonsen <pjsalomonsen@gmail.com>
2017-01-13 20:08:28 +0100

    Upgraded to latest version of Angular

--- materials/devserver.js
+++ materials/devserver.js
@@ -1,4 +1,7 @@
-var express = require('express');
-var app = express();
+"use strict";
+let express = require('express');
+let compression = require('compression');
+let app = express();
+app.use(compression());
 app.use('/', express.static('.'));
 app.listen(3000);
