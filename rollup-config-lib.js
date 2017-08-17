2ab9fa9f369ab647561f8bb873d9cc87470d3a50
Peter Johan Salomonsen <pjsalomonsen@gmail.com>
2017-08-17 21:23:10 +0200

    Can't delete JS just like that since rollup in main app needs it.

--- rollup-config-lib.js
+++ rollup-config-lib.js
@@ -2,35 +2,6 @@ import rollup from 'rollup'
 import fs from 'fs';
 import path from 'path';
 
-const removeJSPlugin = function (options) {
-    return {
-        ongenerate(){
-            let walkSync = (dir, filelist) => {            
-              let files = fs.readdirSync(dir);
-              filelist = filelist || [];
-              files.forEach(function(file) {
-                if (fs.statSync(dir + file).isDirectory()) {
-                  filelist = walkSync(dir + file + '/', filelist);
-                }
-                else {
-                  filelist.push(dir+file);
-                }
-              });
-              return filelist;
-          };
-          walkSync(options.src)
-            .filter(path => 
-                path.indexOf(options.except)===-1 && (
-                    path.indexOf(".js")===path.length-3 ||
-                    path.indexOf(".js.map")===path.length-7
-                )
-            )
-            .forEach(path => fs.unlinkSync(path));
-          
-        }
-    };
-};
-
 export default {
   entry: 'dist/index.js',
   dest: 'dist/fintechneo-angulartemplates.umd.js',
@@ -43,10 +14,7 @@ export default {
     console.warn( warning.message );
   },
   plugins: [
-    removeJSPlugin({
-        src:  'dist/',
-        except: "fintechneo-angulartemplates.umd.js"        
-    })
+    
   ],
   globals: {  
     
