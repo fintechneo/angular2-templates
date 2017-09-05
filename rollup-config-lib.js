c526ca1ccacc48e1b79b18c43e8adef4e0430aa8
Peter Johan Salomonsen <pjsalomonsen@gmail.com>
2017-09-05 11:19:40 +0200

    Angular library setup

--- rollup-config-lib.js
+++ rollup-config-lib.js
@@ -2,19 +2,50 @@ import rollup from 'rollup'
 import fs from 'fs';
 import path from 'path';
 
+const removeJSPlugin = function (options) {
+  return {
+      ongenerate(){
+          let walkSync = (dir, filelist) => {            
+            let files = fs.readdirSync(dir);
+            filelist = filelist || [];
+            files.forEach(function(file) {
+              if (fs.statSync(dir + file).isDirectory()) {
+                filelist = walkSync(dir + file + '/', filelist);
+              }
+              else {
+                filelist.push(dir+file);
+              }
+            });
+            return filelist;
+        };
+        walkSync(options.src)
+          .filter(path => 
+              path.indexOf(options.except)===-1 && (
+                  path.indexOf(".js")===path.length-3 ||
+                  path.indexOf(".js.map")===path.length-7
+              )
+          )
+          .forEach(path => fs.unlinkSync(path));
+        
+      }
+  };
+};
+
 export default {
-  entry: 'dist/index.js',
+  entry: 'dist/fintechneo-angulartemplates.js',
   dest: 'dist/fintechneo-angulartemplates.umd.js',
   sourceMap: false,
   format: 'umd',
   moduleName: 'fintechneo.angulartemplates',
-  onwarn: function(warning) {
-   
+  onwarn: function(warning) {   
     // console.warn everything else
     console.warn( warning.message );
   },
   plugins: [
-    
+    /*removeJSPlugin({
+        src:  'dist/',
+        except: "fintechneo-angulartemplates.umd.js"        
+    })*/
   ],
   globals: {  
     
