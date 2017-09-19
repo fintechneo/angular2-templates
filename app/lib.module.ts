83fdc70b01c689d18318803bb3845569b3656550
paul <paul@Candy.local>
2017-09-19 09:51:48 +0100

    tidy up

--- app/lib.module.ts
+++ app/lib.module.ts
@@ -1,23 +1,11 @@
-import { NgModule, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
 import { OrgPanelBoardComponent } from './orgpanelboard/orgpanelboard.component';
-
-/*
-@Component({
-    selector: "ftn-orgpanelboard",
-    template: `<h1> Board Members </h1> `
-})
-
-export class OrgPanelBoardComponent {
-    constructor() {
-    }
-}
-*/
 
 
 @NgModule({
     imports: [
-       CommonModule
+  
     ],
     declarations: [OrgPanelBoardComponent
     ],
